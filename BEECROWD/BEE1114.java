8a7960b426b202e9908d75ecd9b191aeb6af0ea7
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2025-04-01 23:55:10 -0300

    feat: adicionado resolução para o problema 1114 do beecrowd.

---
+++ BEECROWD/BEE1114.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class BEE1114{
+    public static void main(String args[]){
+        Scanner teclado = new Scanner(System.in);
+            int senha;
+           
+           do{
+            senha = teclado.nextInt();
+            if(senha == 2002){
+                System.out.println("Acesso Permitido");
+            }
+            else{
+                System.out.println("Senha Invalida");
+            }
+
+           }while(senha != 2002);
+               
+        teclado.close();
+    }    
+}
