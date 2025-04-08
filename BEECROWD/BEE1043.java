ab073d0cdd6f66a1b075dad8a14d7f90b2c63229
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2025-04-07 23:11:04 -0300

    feat: adicionado resolucao para o problema 1043 do Beecrowd

--- BEECROWD/BEE1043.java
+++ BEECROWD/BEE1043.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class BEE1043 {
+import java.util.Scanner;
+
+public class BEE1043 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        double area;
+        double perimetro;
+        double a = teclado.nextDouble();
+        double b = teclado.nextDouble();
+        double c = teclado.nextDouble();
+        boolean condicaoTriangulo = a + b > c && a + c > b && b + c > a;
+
+
+        if (condicaoTriangulo) {
+            perimetro = a + b + c;
+
+            System.out.printf("Perimetro = %.1f\n", perimetro);
+        } else {
+            area = (a + b) * c / 2;
+            System.out.printf("Area = %.1f\n", area);
+        }
+        teclado.close();
+    }
 }
